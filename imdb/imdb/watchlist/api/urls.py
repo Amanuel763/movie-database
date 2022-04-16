5d31f3cf573846bec2fd07daa3ab5184a3ce31e1
Amanuel Afework <schwartzfuss763@yahoo.com>
2022-04-16 13:15:04 -0500

    updated serializers, url, models and tested application

--- imdb/imdb/watchlist/api/urls.py
+++ imdb/imdb/watchlist/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 # from watchlist.api.views import movie_list, movie_details
-from watchlist.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV
+from watchlist.api.views import ReviewList, WatchListAV, WatchDetailAV, StreamPlatformAV, ReviewDetail
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
-
+    path('review', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 ]
